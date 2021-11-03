853aff8dafadda1122596b75f240f55139029295
akshay14ce1078 <deoghare.akshay.14ce1078@gmail.com>
2021-11-03 16:40:06 +0530

    feat: implement put patch and delete api

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 function routes (Book) {
   const bookRouter = express.Router();
-  bookRouter
-  .post('/books',(req,res)=>{
+  bookRouter.route('/books')
+  .post((req,res)=>{
     const book = new Book(req.body);
     book.save();
     res.status(201).json(book);
   })
-  .get('/books', (req, res) => {
+  .get((req, res) => {
     const {query} = req
     Book.find(query,(err,books)=>{
       if(err){
@@ -17,16 +17,59 @@ function routes (Book) {
       return res.json(books);
     })
   });
-
-  bookRouter.get('/books/:bookId', (req, res) => {
+  
+  // middleware
+  bookRouter.use('/books/:bookId',(req, res, next)=>{
     const id = req.params.bookId
-    Book.findById(id,(err,books)=>{
-      if(err){
-        return res.send(err);
-      }
-      return res.json(books);
+      Book.findById(id, (err, book)=>{
+        if(err){
+          return res.send(err);
+        }
+
+        if(book) {
+          req.book = book
+          return next();
+        }
+
+        return res.sendStatus(401);
+      })
+  })
+
+  bookRouter.route('/books/:bookId')
+    .get((req, res) => res.json(req.book))
+    .put((req,res) => {
+      const {book} = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      book.save();
+
+      return res.json(book);
     })
-  });
+    .patch((req, res) => {
+      const {book} = req;
+
+      if(req.body._id){
+        delete req.body._id
+      }
+      Object.entries(req.body).forEach(([key, value]) => {
+        book[key] = value;
+      });
+      book.save();
+
+      console.log(book);
+
+      return res.json(book);
+    })
+    .delete((req, res)=>{
+      req.book.remove((err)=>{
+        if(err) {
+          return res.send(err)
+        }
+        return res.sendStatus(204);
+      })
+    })
 
   return bookRouter
 }
